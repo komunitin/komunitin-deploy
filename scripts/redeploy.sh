2fa31549ef1cd1cce64d3d3691e9f8a13fdd820a
Esteve <badia.orive@gmail.com>
2020-03-24 18:59:49 +0100

    Use mirage mocking

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -3,4 +3,4 @@
 docker stop komunitin-app
 docker system prune -f
 docker pull komunitin/komunitin-app:latest
-docker run -p 2030:80 -d --name=komunitin-app komunitin/komunitin-app:latest
+docker run -p 2030:80 -d --name=komunitin-app --env USE_MIRAGE=200 komunitin/komunitin-app:latest
