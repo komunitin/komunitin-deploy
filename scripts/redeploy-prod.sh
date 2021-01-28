baf6bcb1f6b4745e9a9964e0c3ebf3cb615bf097
Esteve <badia.orive@gmail.com>
2021-01-28 10:18:30 +0100

    Update redeploy-prod.sh

--- scripts/redeploy-prod.sh
+++ scripts/redeploy-prod.sh
@@ -3,4 +3,4 @@
 docker stop komunitin-app
 docker system prune -f
 docker pull komunitin/komunitin-app:latest
-docker run -p 2030:80 -d --name=komunitin-app --restart unless-stopped komunitin/komunitin-app:latest
+docker run -p 2028:80 -d --name=komunitin-app --restart unless-stopped komunitin/komunitin-app:latest
