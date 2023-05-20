8bfb66bbc8af5931698e2c52e1153cc2dd3737b5
Esteve Badia <badia.orive@gmail.com>
2023-05-20 17:14:13 +0200

    Deploy ices from here

---
+++ scripts/redeploy-ices-demo.sh
@@ -0,0 +1,33 @@
+# Reinstall IntegralCES demo.
+
+export SITE_NAME="IntegralCES Demo"
+export ADMIN_PASSWORD="demo@ices2030"
+export MYSQL_PASSWORD="demo@ices2030"
+
+cd /opt/ices
+docker-compose down --volumes
+git pull
+docker-compose up -d --build
+./install.sh --demo
+
+# Add settings for proxy-pass and mail
+docker-compose exec integralces printf "\n\
+// Configure reverse proxy.\n\
+$conf['reverse_proxy'] = TRUE;\n\
+$conf['reverse_proxy_addresses'] = ['172.17.0.1'];\n\
+$base_url = 'https://demo.integralces.net';\n\
+\n\
+// Configure mailing (disabled)\n\
+$conf['smtp_allowhtml'] = 1;\n\
+$conf['smtp_deliver'] = 0;\n\
+$conf['smtp_on'] = 1;\n\
+" >> sites/default/settings.php
+
+# Add front page message.
+FRONT_DEMO_CONTENT = "\n\
+  <p style='color:black;'><i><?php print t('This is a demo site. Login as user:'); ?><br>\n\
+  <?php print t('NET1: Gauss, Euclides, Riemann (admin).'); ?><br>\n\
+  <?php print t('NET2: Gauss, Noether, Fermat (admin).'); ?><br>\n\
+  <?php print t('All passwords are "integralces"'); ?></i></p>\n\
+"
+RUN sed -i "/social currencies management for communities./a $FRONT_DEMO_CONTENT" sites/all/themes/greences/templates/page.tpl.php
--- scripts/redeploy-test.sh
+++ scripts/redeploy-test.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 docker-compose --project-name komunitin-test --file /opt/komunitin-deploy/docker-compose.test.yml down --volumes
 docker system prune -f
+
 docker-compose --file /opt/komunitin-deploy/docker-compose.test.yml pull
 docker-compose --project-name komunitin-test --file /opt/komunitin-deploy/docker-compose.test.yml up -d --build
