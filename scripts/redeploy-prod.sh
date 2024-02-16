ae18d38451aa2fd336394b727ad045aaa7f3c3e8
root <root@vps781391.ovh.net>
2024-02-16 12:48:53 +0100

    restart redis

--- scripts/redeploy-prod.sh
+++ scripts/redeploy-prod.sh
@@ -2,3 +2,4 @@
 
 docker compose --file /opt/komunitin-deploy/docker-compose.prod.yml pull
 docker compose --project-name komunitin-prod --file /opt/komunitin-deploy/docker-compose.prod.yml up -d --build
+
